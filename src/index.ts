c34b37119be250131d7196a630d6a27ebaa1edde
jianghua1 <719584530@qq.com>
2024-09-05 19:23:14 +0800

    333

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const sum = (a: number, b: number): number => a + b;
+const sum = (a: number, b: number): number => a + b
 
-const result = sum(3, 3);
-
-console.log(result);
-
-export { sum };
+const result = sum(3, 3)
+const result2 = sum(3, 3)
+console.log(result)
+console.log(result2)
+export { sum }
